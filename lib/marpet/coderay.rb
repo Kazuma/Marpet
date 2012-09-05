69bdad515d0829818a8906dc0b900911b6f72a3f
Kazuma <muramatu.k@gmail.com>
2012-09-06 01:29:23 +0900

    add default lang

--- lib/marpet/coderay.rb
+++ lib/marpet/coderay.rb
@@ -3,6 +3,9 @@ require 'coderay'
 
 class Coderay < Redcarpet::Render::HTML
   def block_code(code, language)
+    if language.nil?
+      language = :text
+    end
     CodeRay.scan(code, language).div
   end
 end
